12ed9649f36f4f8a051a0b20e413f02f88ae7fab
sseq007 <sseq007@naver.com>
2023-07-09 19:52:01 +0900

    김영환 JPA 강의 지연 로딩과 조회 서능 최적화 주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -82,4 +82,13 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllwithmemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
